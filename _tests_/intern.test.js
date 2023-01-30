99e4d857d10c84fd4ecd31f9d72e6e8b0bf9d1b3
Danielle Cavinder <daniellecavinder@gmail.com>
2023-01-30 10:42:31 -0700

    added other tests, fixed manager bugs

--- _tests_/intern.test.js
+++ _tests_/intern.test.js
@@ -1,1 +1,33 @@
 // intern tests
+const Intern = require('../lib/intern')
+
+describe('Intern class', () => {
+    it("getName returns the name for the specified class.", () => {
+        const testInternName = 'Samantha';
+        const testIntern = new Intern('Samantha',5678, 'sammy@email.com', 'University of Denver');
+        expect(testIntern.name).toBe(testInternName);
+    });
+
+    it("getId returns the id for the specified class.", () => {
+        const testInternId =5678;
+        const testIntern = new Intern('Samantha',5678, 'sammy@email.com', 'University of Denver');
+        expect(testIntern.id).toBe(testInternId);
+    });
+
+    it("getEmail returns the email for the specified class.", () => {
+        const testInternEmail = 'sammy@email.com';
+        const testIntern = new Intern('Samantha',5678, 'sammy@email.com', 'University of Denver');
+        expect(testIntern.email).toBe(testInternEmail);
+    });
+
+    it("getSchool returns the school for the specified class.", () => {
+        const testInternSchool = 'University of Denver';
+        const testIntern = new Intern('Samantha',5678, 'sammy@email.com', 'University of Denver');
+        expect(testIntern.school).toBe(testInternSchool);
+    });
+
+    it("getRole returns 'Intern'.", () => {
+        const testIntern = new Intern('Samantha',5678, 'sammy@email.com', 'University of Denver');
+        expect(testIntern.getRole()).toBe('Intern');
+    });
+});
--- _tests_/manager.test.js
+++ _tests_/manager.test.js
@@ -1,1 +1,33 @@
 // manager tests
+const Manager = require('../lib/manager');
+
+describe('Manager class', () => {
+    it("getName returns the name for the specified class..", () => {
+        const testManagerName = 'Jennifer';
+        const testManager = new Manager('Jennifer', 3456, 'jenny@email.com', 256);
+        expect(testManager.name).toBe(testManagerName);
+    });
+
+    it("getId returns the id for the specified class.", () => {
+        const testManagerId = 3456;
+        const testManager = new Manager('Jennifer', 3456, 'jenny@email.com', 256);
+        expect(testManager.id).toBe(testManagerId);
+    });
+
+    it("getEmail returns the email for the specified class.", () => {
+        const testManagerEmail = 'jenny@email.com';
+        const testManager = new Manager('Jennifer', 3456, 'jenny@email.com', 256);
+        expect(testManager.email).toBe(testManagerEmail);
+    });
+
+    it("getOfficeNumber returns the office number for the specified class.", () => {
+        const testOfficeNumber = 256;
+        const testManager = new Manager('Jennifer', 3456, 'jenny@email.com', 256);
+        expect(testManager.officeNumber).toBe(testOfficeNumber);
+    });
+
+    it("getRole returns 'Manager'.", () => {
+        const testManager = new Manager('Jennifer', 3456, 'jenny@email.com', 256);
+        expect(testManager.getRole()).toBe('Manager');
+    });
+});
